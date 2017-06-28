60cc916ea1b67c37a28cb467b149a7750c56ba60
Michał TUlejko <michal.tulejko@gmail.com>
2017-06-29 00:18:25 +0200

    #00 - post method for user registration

--- Front/js/learningScripts.js
+++ Front/js/learningScripts.js
@@ -25,3 +25,25 @@ function HandleError(error){
     let errorMessage = error.responseJSON.ResponseStatus.Message;
     alert(errorMessage);
 }
+
+function SubmitRegister(userName, password, fullUserName){
+    let url = `/users/addnew`;
+
+    $.ajax({
+        contentType: "application/json",
+        type: "POST",
+        url: url,
+        data: {
+            UserName: userName,
+            Password: password,
+            FullUserName: fullUserName
+        },
+        success: function(data)
+        {
+                alert("Pomyślnie zalogowano");
+        },
+        error: function(err){
+            HandleError(err);
+        }
+    });
+}
